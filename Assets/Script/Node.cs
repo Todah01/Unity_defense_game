177bc71b6a743a6c2d9cf2334602e97dcf381543
Todah01 <wobo5695@gmail.com>
2023-10-05 18:44:46 +0900

    Fix lfs.

---
+++ Assets/Script/Node.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Node
+{
+    public Vector2Int coodinates;
+    public bool isWalkable;
+    public bool isExplored;
+    public bool isPath;
+    public Node connectedTo;
+
+    public Node(Vector2Int coodinates, bool isWalkable){
+        this.coodinates = coodinates;
+        this.isWalkable = isWalkable;
+    }
+}
